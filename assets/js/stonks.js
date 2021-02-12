31351c978507b51e1d06ddc0ce00ed0d8c08bded
Samuel Lewis <admin@preprocess.uk>
2021-02-12 01:44:14 +0000

    Positions or ban + responsive design

---
+++ assets/js/stonks.js
@@ -0,0 +1,26 @@
+var yfinance = "https://tendie.st/tendies";
+var result = document.getElementById("result");
+var avgPrice = parseFloat(document.getElementById("avg_price").innerText);
+var currentPrice = document.getElementById("current_price");
+var qty = parseFloat(document.getElementById("qty").innerText);
+
+function refreshResult(price) {
+    currentPrice.innerText = "$" + price;
+    var change = ((avgPrice*qty)-(price*qty)).toFixed(2);
+    if (price > avgPrice) {
+        result.innerHTML = '<span class="stonks">$' + -change + '</span>';
+    } else {
+        result.innerHTML = '<span class="oof">-$' + change + '</span>';
+    }
+}
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        var response = JSON.parse(xhr.responseText);
+        var closePrices = response["chart"]["result"][0]["indicators"]["quote"][0]["close"];
+        refreshResult(closePrices[closePrices.length - 1]);
+    }
+}
+xhr.open("GET", yfinance, true);
+xhr.send(null);
